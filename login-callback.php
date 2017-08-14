2283e3bccb0f865d150ab55dd1e6c0c2de871651
dalborgo <marco@dalborgo.it>
2017-08-14 20:26:24 +0200

    dfsfs

--- login-callback.php
+++ login-callback.php
@@ -13,8 +13,9 @@ $fb = new Facebook\Facebook([
 
 $helper = $fb->getRedirectLoginHelper();
 try {
-   // $fb->setExtendedAccessToken();
     $accessToken = $helper->getAccessToken();
+    $mio=$fb->get('359734667794410?fields=access_token',$accessToken);
+    print_r($mio);
 } catch(Facebook\Exceptions\FacebookResponseException $e) {
     // When Graph returns an error
     echo 'Graph returned an error: ' . $e->getMessage();
@@ -25,38 +26,14 @@ try {
     exit;
 }
 
-if (!isset($accessToken)) {
-    if ($helper->getError()) {
-        header('HTTP/1.0 401 Unauthorized');
-        echo "Error: " . $helper->getError() . "\n";
-        echo "Error Code: " . $helper->getErrorCode() . "\n";
-        echo "Error Reason: " . $helper->getErrorReason() . "\n";
-        echo "Error Description: " . $helper->getErrorDescription() . "\n";
-    } else {
-        header('HTTP/1.0 400 Bad Request');
-        echo 'Bad request';
-    }
-    exit;
-}
-$oAuth2Client = $fb->getOAuth2Client();
-if (! $accessToken->isLongLived()) {
-    // Exchanges a short-lived access token for a long-lived one
-    try {
-        $accessToken = $oAuth2Client->getLongLivedAccessToken($accessToken);
-        // Logged in!
-        //$_SESSION['facebook_access_token3'] = (string) $accessToken;
-        $filePath = 'photo/access_token.txt';
-        $file = fopen($filePath, 'w');
-        fwrite($file, (string) $accessToken);
-        fclose($file);
-        header("location: http://localhost:50000");
-        // Now you can redirect to another page and use the
-        // access token from $_SESSION['facebook_access_token']
-    } catch (Facebook\Exceptions\FacebookSDKException $e) {
-        echo "<p>Error getting long-lived access token: " . $e->getMessage() . "</p>\n\n";
-        exit;
-    }
-
-    echo '<h3>Long-lived</h3>';
-    var_dump($accessToken->getValue());
+if (isset($accessToken)) {
+    // Logged in!
+    //$_SESSION['facebook_access_token'] = (string) $accessToken;
+    $filePath = 'photo/access_token.txt';
+    $file = fopen($filePath, 'w');
+    fwrite($file, (string) $mio);
+    fclose($file);
+    //header("location: http://localhost:50000/");
+    // Now you can redirect to another page and use the
+    // access token from $_SESSION['facebook_access_token']
 }
--- prova.php
+++ prova.php
@@ -1,19 +1,31 @@
 <?php
 require __DIR__.'/vendor/autoload.php';
-
-$myfile = fopen("photo/access_token.txt", "r") or die("Unable to open file!");
-$a_token = fread($myfile,filesize("photo/access_token.txt"));
-fclose($myfile);
-
-echo $a_token;
-
+if(!session_id()) {
+    session_start();
+}
+use Facebook\FacebookRequest;
 
 $fb = new Facebook\Facebook([
     'app_id' => '1555174697849814',
     'app_secret' => 'df6dabb17ee347f840910def6f49e11f',
     'default_graph_version' => 'v2.9',
 ]);
+$fbApp = new Facebook\FacebookApp('1555174697849814', 'df6dabb17ee347f840910def6f49e11f');
 
+$request = new FacebookRequest($fbApp, 'EAAWGbFNt09YBAEO8qKXqzLYGbUOUZBjVAT8N7h2yp29uXAroSzDrlTUlIVZBf3DvSm9jxowQTOxEd5lnwI7TreFO5BNdHWFUOa9jdWA55ieWKqpyaDYCyE6HKyeeRkrWNk9bG7OdYn6U87VkZCMsINIY7SmJpEZD', 'GET', '/me/accounts/');
+//$request = $fb->request('GET', '/me/accounts');
 //$fb->get('359734667794410?fields=access_token',$a_token);
-$requestUserName = $fb->request('GET', '/me/accounts', [],$a_token);
-echo print_r($requestUserName);
+try {
+    $response = $fb->getClient()->sendRequest($request);
+    echo 'miao';
+} catch(Facebook\Exceptions\FacebookResponseException $e) {
+    // When Graph returns an error
+    echo 'Graph returned an error: ' . $e->getMessage();
+    exit;
+} catch(Facebook\Exceptions\FacebookSDKException $e) {
+    // When validation fails or other local issues
+    echo 'Facebook SDK returned an error: ' . $e->getMessage();
+    exit;
+}
+
+$graphNode = $response->getGraphNode();
